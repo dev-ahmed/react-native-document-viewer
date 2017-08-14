57059c70192467e2ed3b6a686903011e41d020cd
Mohamed Mamdouh <mohamed.mamdouh@mowesolutions.com>
2017-08-14 10:28:46 +0300

    show seperators between Pages

--- index.js
+++ index.js
@@ -167,15 +167,12 @@ export default class RNDocumentViewer extends Component {
                         <TouchableOpacity style={styles.zoomBtns} onPress={() => this.zoomOut()}>
                             <Text style={[styles.text, styles.zoomIcons]}>-</Text>
                         </TouchableOpacity>
-
                         <View style={[styles.btn, styles.zoomSegment]}>
                             <Text style={[styles.text, { fontSize: 18, color: 'white' }]}>|</Text>
                         </View>
-
                         <TouchableOpacity style={[styles.zoomBtns, { marginRight: 10 }]} onPress={() => this.zoomIn()}>
                             <Text style={[styles.text, styles.zoomIcons]}>+</Text>
                         </TouchableOpacity>
-
                         <View style={[styles.btn, { width: 80 }]}>
                             <Text style={[styles.text, { width: 80, fontSize: 14 }]}> Zoom</Text>
                         </View>
@@ -206,7 +203,9 @@ export default class RNDocumentViewer extends Component {
                     onError={(error) => {
                         console.log(error);
                     }}
-                    style={styles.pdf} />
+                    style={styles.pdf}
+                    spacing={150}
+                    />
             </Modal>
         )
     }
@@ -223,4 +222,3 @@ export default class RNDocumentViewer extends Component {
             })
     }
 }
-
